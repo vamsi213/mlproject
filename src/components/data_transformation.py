58f42228feaf1472b3123c8c8e3f4a0fcf3ad76a
vamsi bh <vamsi.bh12@gmail.com>
2024-01-10 17:09:16 +0530

    Model training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -122,7 +122,7 @@ class DataTransformation:
             return (
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path,
+                
             )
         except Exception as e:
             raise CustomException(e,sys)
